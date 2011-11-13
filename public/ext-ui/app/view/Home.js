69d5d706f48d4cdb3e20080573efd99fb80779b6
Adrian Arias <aarias239@gmail.com>
2011-11-13 14:55:43 -0500

    ext-ui home view

---
+++ public/ext-ui/app/view/Home.js
@@ -0,0 +1,37 @@
+Ext.define('FashionHelper.view.Home', {
+	extend: 'Ext.container.Container',
+	alias: 'widget.home',
+	width: 500,
+	height: 400,
+	layout: {
+		type: 'vbox',
+		align: 'center',
+		pack: 'center'
+	},
+	autoRender: true,
+	autoShow: true,
+
+	initComponent: function(){
+		
+		this.items = [{
+			xtype: 'component',
+			autoEl: {
+				tag: 'p',
+				html: 'Lorem Ipsum is simply dummy text of the \
+				printing and typesetting industry. Lorem Ipsum \
+				has been the industry standard dummy text \
+				ever since the 1500s, when an unknown printer \
+				took a galley of type and scrambled it to make \
+				a type specimen book. It has survived not only \
+				five centuries, but also the leap into electronic \
+				typesetting, remaining essentially unchanged. \
+				It was popularised in the 1960s with the release \
+				of Letraset sheets containing Lorem Ipsum passages, \
+				and more recently with desktop publishing software \
+				like Aldus PageMaker including versions of Lorem Ipsum.'
+			}
+		}];
+
+		this.callParent(arguments);
+	}
+});
--- public/ext-ui/app/view/Viewport.js
+++ public/ext-ui/app/view/Viewport.js
@@ -2,14 +2,17 @@ Ext.define('FashionHelper.view.Viewport', {
 	extend: 'Ext.Panel',
 
 	requires: [
-		'FashionHelper.view.Navigation'
+		'FashionHelper.view.Navigation',
+		'FashionHelper.view.Home'
 	],
 
 	initComponent: function() {
 		this.items = [{
 			xtype: 'navigation'
-		}]
+		}, {
+			xtype: 'home'
+		}];
 		
-		this.callParent();
+		this.callParent(arguments);
 	}
 });
