7c76ceca7bd7d3c9e0a59e357b98cd85b0fe9087
Adrian Arias <aarias239@gmail.com>
2011-11-21 15:49:39 -0500

    improvements to the navigation widget and various layouts

--- public/ext-ui/app/view/Portal.js
+++ public/ext-ui/app/view/Portal.js
@@ -1,21 +1,28 @@
 Ext.define('FashionHelper.view.Portal', {
-	extend: 'Ext.panel.Panel',
-	alias: 'widget.portal',
+	extend: 'Ext.container.Container',
+	alias: 'widget.portalcomp',
 	autoRender: true,
 	autoShow: true,
 	autoDestroy: true,
 	title: 'Admin Portal',
+	layout: 'anchor',
 
 	requires: [,
-		'FashionHelper.view.Home'
+		'FashionHelper.view.Home',
+		'FashionHelper.view.Brand'
 	],
 
-	initComponent: function(){
-		
-		this.items = [{
-			xtype: 'home'
-		}];
+	items: [{
+		xtype: 'homecomp',
+		anchor: '100%'
+	}],
 
+	onRender: function(){
+		//this.items = Ext.apply({}, this.initialConfig,this.items);
 		this.callParent(arguments);
-	}
+	},
+
+	// initComponent: function(){
+	// 	console.log('portal initialized');
+	// }
 });
--- public/ext-ui/app/view/Viewport.js
+++ public/ext-ui/app/view/Viewport.js
@@ -1,46 +1,61 @@
 Ext.define('FashionHelper.view.Viewport', {
 	extend: 'Ext.Panel',
-	alias: 'widget.viewport-comp',
+	alias: 'widget.viewportcomp',
+	layout: 'anchor',
 
 	requires: [
 		'FashionHelper.view.Navigation',
 		'FashionHelper.view.Portal'
 	],
-        refs: [
-            {
-                ref: 'table'
-            }
-            
-        ],
-        items: [
-            {
-                xtype: 'navigation'
-            },
-            {
-                id: 'globalTable',
-                xtype: 'container',
-                title: 'Default Table',
-                width: '100%',
-                flex: 2
-            },
-            {
-                id: 'globalForm',
-                xtype: 'container',
-                title: 'Default Form',
-                width: '100%',
-                flex: 1
-            },
-            {
-                xtype: 'portal',
-                id: 'globalPortal'
-            }
-        ],
+	refs: [
+		{
+			ref: 'table'
+		}
+		
+	],
+	items: [
+		{
+			xtype: 'container',
+			anchor: '100%',
+			layout: {
+				type: 'hbox',
+				align: 'center',
+				pack: 'center'
+			},
+			items: [{
+				xtype: 'navigationcomp',
+				flex: 1,
+				id: 'navigation',
+				width: '100%'
+			}, {
+				xtype: 'portalcomp',
+				flex: 4,
+				id: 'globalPortal',
+				// width: '100%',
+			}]
+			
+		},
+		{
+			id: 'globalTable',
+			xtype: 'container',
+			title: 'Default Table',
+			width: '100%',
+			flex: 2
+		},
+		{
+			id: 'globalForm',
+			xtype: 'container',
+			title: 'Default Form',
+			width: '100%',
+			flex: 1
+		},
+	],
 
 	initComponent: function() {
-            
-            this.callParent(arguments);
-            
-            this.down('#globalTable').hide();
-            this.down('#globalForm').hide();
+		
+		// Generates error
+		// this.down('#globalTable').hide();
+		// this.down('#globalForm').hide();
+		this.callParent(arguments);
 	}
 });
