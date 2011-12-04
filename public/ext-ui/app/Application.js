a19808add70f30096afc76f302e79b1b2ed63795
Adrian Arias <aarias239@gmail.com>
2011-12-03 19:17:16 -0500

    removing leftovers from main extjs app

--- public/ext-ui/app/Application.js
+++ public/ext-ui/app/Application.js
@@ -3,21 +3,6 @@ Ext.application({
     name: 'FashionHelper',
     appFolder: '/ext-ui/app',
     autoCreateViewport: true,
-
-    data: {},
-    
-    setData: function(key, value)
-    {
-        this.data.key = value;
-    },
-    getData: function(key)
-    {
-        return this.data.key;
-    },
-    hasData: function(key)
-    {
-        return typeof this.data.key != "undefined";
-    },
     
     controllers: [
         'Navigation',
